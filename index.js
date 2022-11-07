a3597b8481d2c013e0bc0ae6aabee04988e0149b
Caaron <arn_0000@hotmail.com>
2022-11-07 15:31:22 +0800

    edit3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8123;
 const cors = require("cors");
 
 require("dotenv").config();
